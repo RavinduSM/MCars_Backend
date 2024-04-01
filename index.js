d2027e02f9f473acce49de03f41eaf37c6e4f552
Ravindu Muthukudaarachchi <70681852+RavinduSM@users.noreply.github.com>
2024-04-01 09:23:15 +0530

    Update cors
    updated cors to the link in the backend

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // Enable cors at the server side.
 
 const corsOption = {
-  origin: ["http://localhost:3000"],
+  origin: ["https://mcars-backend.onrender.com"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
