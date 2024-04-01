a84e830b0ab6626bced268eaf6383d895da9fd23
Ravindu Muthukudaarachchi <70681852+RavinduSM@users.noreply.github.com>
2024-04-01 09:28:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // Enable cors at the server side.
 
 const corsOption = {
-  origin: ["https://mcars-backend.onrender.com"],
+  origin: ['http://localhost:3000'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
