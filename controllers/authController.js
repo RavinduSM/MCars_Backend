c807cfb0d93df972bfbc28c05f200da320737b8a
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2024-05-11 19:04:13 +0530

    updated the user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,44 @@
 import User from "../models/userModel.js";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import asyncHandler from "../middleware/asyncHandler.js";
 import createToken from "../utils/createToken.js";
 
-export const signup = async (req, res) => {
-  const { username, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, password: hashedPassword });
+export const signup = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the fields");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("User already exists");
+
+  // Hash the user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User Created successfully");
+    createToken(res, newUser._id);
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(400);
+    throw new Error("Invalid user data");
   }
-};
+});
 
 export const signin = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ username });
+  const existingUser = await User.findOne({ email });
 
   if (existingUser) {
-    const isPasswordValid = await bcryptjs.compare(
+    const isPasswordValid = await bcrypt.compare(
       password,
       existingUser.password
     );
@@ -32,6 +49,8 @@ export const signin = asyncHandler(async (req, res) => {
       res.status(201).json({
         _id: existingUser._id,
         username: existingUser.username,
+        email: existingUser.email,
+        isAdmin: existingUser.isAdmin,
       });
     } else {
       res.status(401).json({ message: "Invalid Password" });
--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,28 @@
 import mongoose from "mongoose";
 
-const userShcema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userShcema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userShcema);
 export default User;
