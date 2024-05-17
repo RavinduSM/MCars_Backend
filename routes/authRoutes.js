d1c95e0b080e9bcc2512224c733e3f5ba7a37e50
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2024-05-17 08:59:42 +0530

    getCurrentUserProfile and updateCurrentUserProfile

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,23 @@
 import express, { Router } from "express";
 import {
+  getCurrentUserProfile,
   logoutCurrentUser,
   signin,
   signup,
+  updateCurrentUserProfile,
 } from "../controllers/authController.js";
+
+// middleware
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const UserRouter = Router();
 
 UserRouter.post("/signup", signup);
 UserRouter.post("/signin", signin);
 UserRouter.post("/logout", logoutCurrentUser);
+
+UserRouter.route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
 
 export default UserRouter;
