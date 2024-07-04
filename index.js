7ea11144e431a45aef2197b6377ff945e8324bfa
Ravindu Muthukudaarachchi <70681852+RavinduSM@users.noreply.github.com>
2024-07-04 20:31:40 +0530

    update cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 // Enable cors at the server side.
 
 const corsOption = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
